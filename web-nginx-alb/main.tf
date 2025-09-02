bb62c33273b6a848dd889d9fe9d20d245dbfa70b
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-09-02 12:10:32 +0800

    updated

--- web-nginx-alb/main.tf
+++ web-nginx-alb/main.tf
@@ -174,7 +174,7 @@ resource "aws_lb_listener" "http" {
 # 将自动伸缩组附加到目标组
 resource "aws_autoscaling_attachment" "asg_attachment" {
   autoscaling_group_name = aws_autoscaling_group.nginx_asg.name
-  lb_target_group_arn   = lb_target_group.nginx_tg.arn
+  lb_target_group_arn   = aws_lb_target_group.nginx_tg.arn
 }
 
 
