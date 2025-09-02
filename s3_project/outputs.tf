6cc101b555ad694b2de231b934ece96f92151c47
Pengchao Ma <pengchaoma@Pengchaos-MacBook-Pro.local>
2025-09-02 12:57:29 +0800

    updated

--- s3_project/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "bucket_name" {
-  description = "The name of the created S3 bucket"
-  value       = aws_s3_bucket.main.bucket
-}
-
-output "bucket_arn" {
-  description = "The ARN of the created S3 bucket"
-  value       = aws_s3_bucket.main.arn
-}
